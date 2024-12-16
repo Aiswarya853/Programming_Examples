b487d09fcc4f8d5966eba213217636df360a7f48
Administrator <Administrator@172.19.3.4>
2024-12-16 20:06:53 +0530

    commit latest changes

--- seleniumLearning/src/webdriverCommandsActions/DropdownUsingSendKeys.java
+++ seleniumLearning/src/webdriverCommandsActions/DropdownUsingSendKeys.java
@@ -16,6 +16,7 @@ public class DropdownUsingSendKeys {
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.driver.chromedriver", "C:/Softwares/chromedriver-win64/chromedriver-win64/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		
 		String url ="https://demoqa.com/automation-practice-form";
 		driver.navigate().to(url);
 		JavascriptExecutor js = (JavascriptExecutor) driver;
